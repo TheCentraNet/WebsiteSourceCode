6c1a5ebc8972879ad21b9052ac0c1632cb4951c3
Keegantek <Keegan@keegantek.com>
2014-06-03 18:27:57 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+Index Page
+=================
+<html>
+<body style="background:#1874cd;>
+<center>
+<div style="background-color:#ffffff;">
+<br />
+
+<br />
+</div>
+
+<table style="background-color:#ffffff; margin-top:300px; width:400px; border:1px solid #00000;">
+<tr><td>
+<center>
+logo
+</center>
+</td>
+</tr>
+</table>
+</center>
+</body>
+</html>
