5d1cb12621bd7e5435b1ec9c0b98346d761b1d8d
Keegantek <Keegan@keegantek.com>
2014-06-05 14:35:31 -0400

    Update index.php
    Trying to figure out color scheme... that's kind of what is messing me up right know.

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 Index Page
 =================
 <html>
-<body style="background:#1874cd;>
+<head>
+<title>TheCentraNet</title>
+<link rel="stylesheet" type="text/css" src="style.css">
+</head>
+<body style="background:#C8C8C8;>
 <center>
-<div style="background-color:#ffffff;">
-<br />
-
-<br />
-</div>
-
+<div style="background-color:#C8C8C8;">
 <table style="background-color:#ffffff; margin-top:.10px; width:400; border:1px solid #00000;">
 <div style="width:1335px;height:100px;border:6px groove white;">
 <tr><td>
