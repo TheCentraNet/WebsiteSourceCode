71247a8048783328fd49fbc37496763fb350e903
Dylan Kiley <Idylankiley@gmail.com>
2014-06-06 09:06:14 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-Index Page
-=================
 <html>
 <head>
 <title>TheCentraNet</title>
@@ -8,8 +6,28 @@ Index Page
 <body>
 <div id="head">
     <center>
-    <table width="95%"><tr><td width="25%">Logo</td><td wdith="25%" align="right"><a href="signin.php" class="button">Sign In</a></td></tds></tr></table></center>
+    <table width="95%" height="80px"><tr><td width="25%"><img src="logo.png"></td><td wdith="25%" align="right"><a href="signin.php" class="button">Sign In</a></td></tds></tr></table></center>
 </div>
-<div id="main"></div>
+<div id="main">
+<center>
+    <table width="85%">
+<tr><td><div style="margin-top:90px;"><div style="width:500px;margin-top:0px;height:300px;"><font style="color:#fff;font:30pt solid;font-family:Georgia;font-weight:bold;"><center><i>Unifying The Internet</i></center></font><div style="background-color:#ffffff;color:#168CCC;-webkit-border-radius:15px;-moz-border-radius:15px;border-radius:15px;padding-top:10px;"><ul style="padding-left:50px;font-weight:bold;font-size:18pt;"><li style="padding-bottom:30px;">All of your social networks - in one place</li><li style="padding-bottom:30px;">Completly Secure</li><li style="padding-bottom:30px;">Completely Customizable</li><li>Custom Notifications</li></ul></div></div></div></td><td align="right" width="400">
+    <div id="login">
+        <form>
+        <table border="0" width="375" style="padding-top:20px;padding-bottom:20px;padding-right:20px;"><tr><td colspan="2"><b>Username<br /></b>
+            <input type="text" height="50px" align="center" style="width:350px;font:20px Georgia; color:#383838; height:40px; padding:10px"  id="username" maxlength="100"><br /><br/></td></tr>
+        <tr><td colspan="2"><b>Password<br /></b>
+<input type="password" style="width:350px;font:20px Georgia; color:#383838; height:40px; padding:5px"  id="password" maxlength="100"><br /><br /></td></tr>
+        <tr><td colspan="2" align="right"><a class="link" href="forgotpass.php">Forgot Password?</a>&nbsp &nbsp<input type="submit" value="Sign In" style="border:0px;font-family:Georgia;" class="button"></td></tr></table>
+        </form>
+    </div>
+    <div id="registerbt"><center><div style="min-height:15px;"></div>
+        <a href="register.php" style="" class="button">Create a fukin account</a>
+        </center>
+    </div>
+</td></tr>
+</table>
+</div>
+</div>
 </body>
 </html>
